a41b807fb362870e0c9490a42a23953c32150d95
cc <1826515410@qq.com>
2020-10-19 17:03:52 +0800

    Revert "first commit"
    This reverts commit 362886716fff0089797b0895621f89838db192f9.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-  },
-  {
-    path: 'article',
-    loadChildren: () => import('./article/article.module').then(m => m.ArticleModule)
-  },
-  {
-    path: 'product',
-    loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
-  },
-  {
-    path: '**',
-    redirectTo: 'user'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
